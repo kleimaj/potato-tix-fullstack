529d5e1f4b3f97e3aa18d74df1071e09f42b8f68
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2021-12-21 13:08:14 -0800

    computes occupancy rate

--- pages/api/tickets/index.js
+++ pages/api/tickets/index.js
@@ -28,7 +28,86 @@ const handler = nc()
       ).map((img) => img.src);
       return { artists, dates, anchors, imgs };
     });
-    res.status(200).json(cal);
+    let resArr = [];
+    for (let i = 0; i < 3; i++) {
+      let artist = cal.artists[i];
+      let date = cal.dates[i];
+      let href = cal.anchors[i];
+      // console.log(cal.anchors.length, cal.artists.length, cal.dates.length)
+      await page.goto(href);
+      // await page.goto(href); // wait for the page to load
+
+      var resMap = await page.evaluate(() => {
+        // get ticket group elements
+        const resultMap = {
+          artist: '',
+          date: '',
+        };
+        const PATIO_COUNT = 26;
+        const INSIDE_COUNT = 65;
+        let ticketCount = 0;
+        const elements = Array.from(
+          document.querySelectorAll('.tribe-tickets__item')
+        );
+        // check if sold out
+        const availablitiy = elements.map((item) =>
+          item.getAttribute('data-available')
+        );
+        availablitiy.forEach((available, idx) => {
+          const isPatio = elements[idx]
+            .querySelector('.tribe-tickets__item__content__title')
+            .textContent.includes('Patio');
+          if (available) {
+            // compute amount of tickets available
+            for (let i = 0; i < 70; i++) {
+              elements[idx]
+                .querySelector('.tribe-tickets__item__quantity__add')
+                .click();
+            }
+            const tixAvailable = parseInt(
+              elements[idx].querySelector('input').value
+            );
+            if (isPatio) {
+              const count = PATIO_COUNT - tixAvailable;
+              ticketCount += count;
+              resultMap['patio'] = count;
+            } else {
+              const count = INSIDE_COUNT - tixAvailable;
+              ticketCount += count;
+              // refactor this
+              if ('set1' in resultMap) {
+                resultMap['set2'] = count;
+              } else {
+                resultMap['set1'] = count;
+              }
+            }
+          } else {
+            //it's sold out, add to ticketCount
+            if (isPatio) {
+              ticketCount += PATIO_COUNT;
+              resultMap['patio'] = PATIO_COUNT;
+            } else {
+              ticketCount += INSIDE_COUNT;
+              // refactor this
+              if ('set1' in resultMap) {
+                resultMap['set2'] = INSIDE_COUNT;
+              } else if (idx == 1) {
+                resultMap['set1'] = INSIDE_COUNT;
+              }
+            }
+          }
+        });
+        resultMap['ticketCount'] = ticketCount;
+        resultMap['occupancyRate'] = ticketCount / 156;
+        return resultMap;
+      });
+      artist = artist.replaceAll('\n', ' ');
+      resMap['artist'] = artist;
+      resMap['date'] = date;
+      resMap['href'] = href;
+      resArr.push(resMap);
+    }
+    res.status(200).json(resArr);
   });
 
 export default handler;
